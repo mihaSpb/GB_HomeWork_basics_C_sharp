b322b9b7210327a48a2dd9b7ee51371d1f78ceb7
mihaSpb <49040093+mihaSpb@users.noreply.github.com>
2020-08-26 02:39:09 +0300

    Add files via upload

---
+++ HomeWork_Start/HomeWork_Start/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security;
+
+namespace HomeWork_Start
+{
+    class Program
+    {
+
+        static void Pause ()
+        {
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            string name;
+
+            name = Environment.UserName;
+            Console.SetCursorPosition(Console.WindowWidth / 2, Console.WindowHeight / 2);
+            Console.WriteLine(name);
+            Console.WriteLine("Hello World!");
+            Pause("Перегрузка");
+        }
+
+        static void Pause (string str)
+        {
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
