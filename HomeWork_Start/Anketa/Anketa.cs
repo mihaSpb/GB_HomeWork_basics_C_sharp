4d6ee312090381097529b2fc9eb4a28843005879
MikhailSpb <mihaspb2007@gmail.com>
2020-08-27 00:57:29 +0300

    update

---
+++ HomeWork_Start/Anketa/Anketa.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ClassLibraryPause;
+
+namespace Anketa
+{
+    class Anketa
+    {
+        //Морозов Михаил
+        //Написать программу «Анкета». Последовательно задаются вопросы(имя, фамилия, возраст, рост, вес). 
+        //В результате вся информация выводится в одну строчку.
+
+        static void Main(string[] args)
+        {
+            int age;
+            int growth;
+            int weight;
+
+            Console.Write("Введите свое имя: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Введите свою фамилию: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Введите свой возраст: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите свой рост: ");
+            growth = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите свой вес: ");
+            weight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(String.Format($"{firstName} {lastName}, возраст: {age}, рост: {growth} вес: {weight}"));
+
+            Class1.Pause("Для выхода из консоли нажмите любую кнопку...");
+        }
+    }
+}
---
+++ HomeWork_Start/Dannye/Dannye.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ClassLibraryPause;
+
+namespace Dannye
+{
+    //Морозов Михаил
+    //Написать программу обмена значениями двух переменных:
+    //а) с использованием третьей переменной;
+    //б) * без использования третьей переменной.
+
+
+    class Dannye
+    {
+        static void Main(string[] args)
+        {
+            int x = 100;
+            int y = 20;
+            int z = x;
+
+            x = y;
+            y = z;
+
+            Console.WriteLine(string.Format($"С использованием третьей переменной: {x} {y}"));
+
+            x = x + y;
+            y = y - x;
+            y = -y;
+            x = x - y;
+
+
+            Console.WriteLine(string.Format($"Без использования третьей переменной: {x} {y}"));
+
+
+            Class1.Pause("Готово, для выхода нажмите любую кнопку...");
+        }
+    }
+}
