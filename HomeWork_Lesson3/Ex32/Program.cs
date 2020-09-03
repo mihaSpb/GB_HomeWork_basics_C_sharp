d582a7c42f62a4c38e416f0c409cde7ee66d1c6a
MikhailSpb <mihaspb2007@gmail.com>
2020-09-03 16:52:00 +0300

    update

---
+++ HomeWork_Lesson3/Ex32/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex32
+{
+    // Морозов Михаил
+    // 2. а) С клавиатуры вводятся числа, пока не будет введен 0 (каждое число в новой строке). 
+    // Требуется подсчитать сумму всех нечетных положительных чисел.Сами числа и сумму вывести на экран, используя tryParse;
+
+    // б) Добавить обработку исключительных ситуаций на то, что могут быть введены некорректные данные.
+    // При возникновении ошибки вывести сообщение.Напишите соответствующую функцию;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите произвольное целое число (0 заканчивает ввод чисел):");
+
+            int sum = 0;
+            int a = int.Parse(Console.ReadLine());
+
+            while (a != 0)
+            {
+                if (a > 0 && a % 2 != 0)
+                {
+                    sum = sum + a;
+                }
+                a = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"Сумма всех введенных нечетных положительных чисел: {sum}");
+
+
+
+
+            Console.WriteLine();
+            Console.ReadKey();
+
+
+        }
+    }
+}
