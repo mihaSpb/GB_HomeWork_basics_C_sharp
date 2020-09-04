a31be507da6d463a84c63410c82f13c14040c110
MikhailSpb <mihaspb2007@gmail.com>
2020-09-05 01:01:23 +0300

    update

--- HomeWork_Lesson3/Ex33/Program.cs
+++ HomeWork_Lesson3/Ex33/Program.cs
@@ -30,16 +30,74 @@ namespace Ex33
 
 
 
+
+        // Обработка исключений и инициализация дробей
+        static Fractions Init(int numerator, int denumerator)
+        {
+            bool checkException;
+            Fractions fraction = null;
+
+            do
+            {
+                checkException = false;
+                try
+                {
+                    fraction = new Fractions(numerator, denumerator);
+                }
+                catch (ArgumentException ex)
+                {
+                    checkException = true;
+                    Console.WriteLine("Ошибка: " + ex.Message);
+                    Console.Write("Повторно введите знаменатель дроби: ");
+                    denumerator = CheckInt();
+                }
+            } while (checkException);
+
+            return fraction;
+        }
+
+        static int CommonDenomin(int a, int b)
+        {
+            int comDen;
+
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                {
+                    a = a % b;
+                }
+                else
+                {
+                    b = b % a;
+                }
+            }
+
+            return comDen = a + b;
+        }
+
+
+
         static void Main(string[] args)
         {
             Console.Write("Введите числитель первой дроби: ");
             int n1 = CheckInt();
             Console.Write("Введите знаминатель первой дроби: ");
             int d1 = CheckInt();
-            Console.Write("Введите числитель второй дроби: ");
+            Console.Write("\nВведите числитель второй дроби: ");
             int n2 = CheckInt();
             Console.Write("Введите знаминатель второй дроби: ");
             int d2 = CheckInt();
+
+            Fractions fraction1 = Init(n1, d1);
+            Fractions fraction2 = Init(n2, d2);
+
+            Console.WriteLine($"\nПервая дробь: {fraction1}");
+            Console.WriteLine($"Вторая дробь: {fraction2}");
+
+            Console.WriteLine($"\nСумма двух дробей: {Fractions.Sum(fraction1, fraction2)}");
+            Console.WriteLine($"\nРазность двух дробей: {Fractions.Minus(fraction1, fraction2)}");
+            Console.WriteLine($"\nРезультат умножения двух дробей: {Fractions.Multi(fraction1, fraction2)}");
+            Console.WriteLine($"\nРезультат деления двух дробей: {Fractions.Division(fraction1, fraction2)}");
 
 
 
