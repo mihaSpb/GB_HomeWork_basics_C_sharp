aeab50df81aa0db697e5f719542f01771dbb841a
MikhailSpb <mihaspb2007@gmail.com>
2020-09-06 02:57:00 +0300

    update

---
+++ HomeWork_Start/ClassLibraryPrint/ClassPrintText.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassLibraryPrint
+{
+    public class ClassPrintText
+    {
+        //Морозов Михаил
+        //*Создать класс с методами, которые могут пригодиться в вашей учебе(Print, Pause).
+
+        static public void PrintUserName()
+        {
+            string name = "Михаил";
+            string lastName = "Морозов";
+            string city = "Санкт-Петербург";
+
+            Console.SetCursorPosition(Console.WindowWidth / 2, Console.WindowHeight / 2);
+            Console.WriteLine($"{name} {lastName} из города {city}");
+        }
+    }
+}
