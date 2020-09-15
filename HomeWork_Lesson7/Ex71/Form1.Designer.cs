f620b693783b7be8f4b7f1fac7256b41809b6bfb
MikhailSpb <mihaspb2007@gmail.com>
2020-09-16 00:35:16 +0300

    update

--- HomeWork_Lesson7/Ex71/Form1.Designer.cs
+++ HomeWork_Lesson7/Ex71/Form1.Designer.cs
@@ -28,13 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.btnCommand1 = new System.Windows.Forms.Button();
+            this.btnCommand2 = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(92, 45);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(46, 17);
+            this.lblNumber.TabIndex = 0;
+            this.lblNumber.Text = "label1";
+            // 
+            // btnCommand1
+            // 
+            this.btnCommand1.Location = new System.Drawing.Point(559, 59);
+            this.btnCommand1.Name = "btnCommand1";
+            this.btnCommand1.Size = new System.Drawing.Size(75, 23);
+            this.btnCommand1.TabIndex = 1;
+            this.btnCommand1.Text = "button1";
+            this.btnCommand1.UseVisualStyleBackColor = true;
+            // 
+            // btnCommand2
+            // 
+            this.btnCommand2.Location = new System.Drawing.Point(559, 118);
+            this.btnCommand2.Name = "btnCommand2";
+            this.btnCommand2.Size = new System.Drawing.Size(75, 23);
+            this.btnCommand2.TabIndex = 2;
+            this.btnCommand2.Text = "button2";
+            this.btnCommand2.UseVisualStyleBackColor = true;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(559, 182);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "button3";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCommand2);
+            this.Controls.Add(this.btnCommand1);
+            this.Controls.Add(this.lblNumber);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Button btnCommand1;
+        private System.Windows.Forms.Button btnCommand2;
+        private System.Windows.Forms.Button btnReset;
     }
 }
 
