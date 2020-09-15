672e39413aa3b937af1a03509177a35e22922c3c
MikhailSpb <mihaspb2007@gmail.com>
2020-09-15 16:28:01 +0300

    update

---
+++ HomeWork_Lesson6/Ex61/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Морозов Михаил
+// Изменить программу вывода таблицы функции так, чтобы можно было передавать функции типа double (double, double). 
+// Продемонстрировать работу на функции с функцией a* x^2 и функцией a* sin(x).
+
+namespace Ex61
+{
+    /// <summary>
+    /// Делегат с сигнатурой методов типа double (double, double).
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="x"></param>
+    /// <returns></returns>
+    public delegate double Func(double a, double x);
+
+
+    class Program
+    {
+
+        /// <summary>
+        /// Метод для расчета функции a* x^2
+        /// </summary>
+        /// <param name="a">параметр а</param>
+        /// <param name="x">параметр х</param>
+        /// <returns></returns>
+        public static Double FuncDemo (double a, double x)
+        {
+
+
+            return a * Math.Pow(x,2.0);
+        }
+
+        /// <summary>
+        /// Метод для расчета функции a* sin(x)
+        /// </summary>
+        /// <param name="a"> параметр а </param>
+        /// <param name="x"> параметр х </param>
+        /// <returns></returns>
+        public static Double FuncSin(double a, double x)
+        {
+            return a * Math.Sin(x);
+        }
+
+
+
+
+        static void Plot(List<Func> temp, int start, int end)
+        {
+
+            for (int i = 0; i < temp.Count; i++)
+            {
+                for (int x = start; x <= end; x++)
+                {
+                    Console.WriteLine($"f({x,2}) = {temp[i](x),5}");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            List<Func> funcs = new List<Func>() {new Func(FuncDemo), new Func(FuncSin)};
+
+            Plot(funcs, 5, 10);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWork_Lesson6/Ex61/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Ex61")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Ex61")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("5df844b5-b08f-47a4-84d7-be7787545ef9")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
